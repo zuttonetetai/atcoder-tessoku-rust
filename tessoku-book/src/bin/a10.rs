96e5aef6365864c08e168ee2e70b8ab83f28b95e
netetai <82562404+zuttonetetai@users.noreply.github.com>
2023-08-19 16:24:50 +0900

    add a10

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+        d: usize,
+        lr: [(usize, usize); d],
+    }
+    for i in 0..d {
+        let aa = &a[..lr[i].0 -1];
+        let bb = &a[lr[i].1..];
+        let aamax = aa.iter().max().unwrap();
+        let bbmax = bb.iter().max().unwrap();
+        println!("{:?}", aamax.max(bbmax));
+    }
 }
