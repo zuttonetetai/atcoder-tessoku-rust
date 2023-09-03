0ac9552556713e6f538d29f8be1e99f48cb7a475
netetai <82562404+zuttonetetai@users.noreply.github.com>
2023-09-03 23:56:42 +0900

    add a12

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
+// 答えは10^9秒を超えないこととする。
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
+    }
+    let mut t_min: usize = 1;
+    let mut t_max = 10usize.pow(9);
+    while t_min < t_max {
+        let mid = (t_min + t_max) / 2;
+        let mut print_num = 0;
+        for i in 0..n {
+            print_num += mid / a[i];
+        }
+        if print_num < k {
+            t_min = mid+1 ;
+        } else {
+            t_max = mid;
+        }
+        //println!("{} {} {}", t_max, t_min, print_num);
+    }
+    println!("{}", t_max);
 }
